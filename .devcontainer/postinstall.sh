51b415eb57da6ea351da4309d8b4c97541ebda08
Nathan Bush <nathanbush46@gmail.com>
2024-11-26 20:05:43 -0500

    done

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -1,7 +1,8 @@
 # These commands are run after the docker image is built.
 pip3 install attrs packaging editables pandas scikit-learn sortedcontainers gmpy2 matplotlib
 #pip3 install gosdt
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 pip install -q torch-geometric
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install --upgrade transformers
+pip install --upgrade ipywidgets
